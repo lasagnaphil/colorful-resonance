8f7a7c438a76c259dc61e220461ca096f4d884cf
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-04-06 13:26:30 +0900

    Base code (Tile and TileManager)

---
+++ Assets/Tile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TileType
+{
+    None, Yellow, Blue, Green, Red, Purple, Black
+}
+public class Tile : MonoBehaviour
+{
+    public TileType Type { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+
+	void Start ()
+	{
+	    transform.position = new Vector3(X, Y);
+	}
+}
---
+++ Assets/TileManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour
+{
+    private Tile[,] tiles;
+    public int width;
+    public int height;
+
+    public Tile tilePrefab;
+
+    protected void Start()
+    {
+        Tile[,] tiles = new Tile[width, height];
+        for (int j = 0; j < height; j++)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                tiles[i,j] = Instantiate(tilePrefab) as Tile;
+            }
+        }
+        // Hardcode the map (just for testing)
+        for (int j = 0; j < height; j++)
+        {
+            if (j == 0 || j == height - 1)
+            {
+                for (int i = 0; i < width; i++) tiles[i, j].Type = TileType.Black;
+            }
+            else
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    if (i == 0 || i == width - 1) tiles[i, j].Type = TileType.Black;
+                    else tiles[i, j].Type = TileType.Blue;
+                }
+            }
+        }
+    }
+}

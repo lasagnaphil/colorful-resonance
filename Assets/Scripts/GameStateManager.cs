64e65727aac37e937473d3fa1924688420df80f1
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-04-11 23:07:35 +0900

    Created game state skeleton

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,24 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using FullInspector;
+using MonsterLove.StateMachine;
 using UnityEngine.UI;
+
+public enum GameState
+{
+    Play,
+    Win,
+    Lose
+}
 
 public class GameStateManager : Singleton<GameStateManager>
 {
     public int TurnNumber { get; private set; }
-    private TileManager tileManager;
+    private StateMachine<GameState> fsm;
 
+    private TileManager tileManager;
     public Text turnNumberText;
 
     protected void Start()
     {
-        ResetTurn();
-        tileManager = TileManager.Instance;
-    }
+        fsm = StateMachine<GameState>.Initialize(this);
+        fsm.ChangeState(GameState.Play);
 
-    protected void Update()
-    {
-        turnNumberText.text = "Turn number : " + TurnNumber;
+        tileManager = TileManager.Instance;
     }
 
     public void NextTurn()
@@ -33,5 +39,39 @@ public class GameStateManager : Singleton<GameStateManager>
         TurnNumber = 0;
 
         // In here : reset the monsters' and objects' position
+    }
+
+    private void Play_Enter()
+    {
+        ResetTurn();
+        Debug.Log("Game Start");
+    }
+
+    private void Play_Update()
+    {
+        turnNumberText.text = "Turn number : " + TurnNumber;
+    }
+
+    private void Play_Exit()
+    {
+        Debug.Log("Game Over");
+    }
+
+    // show lose state text (with the option to restart / go back main menu)
+    private void Lose_Enter()
+    {
+    }
+
+    private void Lose_Update()
+    {
+        // if restart button is pressed then call ResetTurn() and go back to Play State
+    }
+
+    private void Win_Enter()
+    {
+    }
+
+    private void Win_Update()
+    {
     }
 }

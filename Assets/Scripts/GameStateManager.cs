2c769041c5669cdf0a7e1c0b69d246499d616f25
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-04-11 19:11:07 +0900

    Add GameStateManager

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using FullInspector;
+
+public class GameStateManager : Singleton<GameStateManager>
+{
+    public int TurnNumber { get; private set; }
+    private TileManager tileManager;
+
+    protected void Start()
+    {
+        ResetTurn();
+        tileManager = TileManager.Instance;
+    }
+
+    public void NextTurn()
+    {
+        TurnNumber++;
+
+        // In here : update all the monsters and objects
+    }
+
+    public void ResetTurn()
+    {
+        TurnNumber = 0;
+
+        // In here : reset the monsters' and objects' position
+    }
+}

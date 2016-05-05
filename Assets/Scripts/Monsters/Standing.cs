babb32b622034cf270f33a5b26a9cdffff8dc196
EQwon <bluetwintail0906@gmail.com>
2016-05-05 21:07:53 +0900

    Change Name of Standing and UpDown

--- Assets/Scripts/Monsters/Standing.cs
+++ Assets/Scripts/Monsters/Standing.cs
@@ -2,14 +2,9 @@
 {
     public class Standing : Monster
     {
-        public bool isFacingUp = false;
-
         protected override void OnTurn()
         {
             base.OnTurn();
-            if (isFacingUp) Move(pos.X, pos.Y + 1);
-            else Move(pos.X, pos.Y - 1);
-            isFacingUp = !isFacingUp;
         }
     }
 }
--- Assets/Scripts/Monsters/UpDown.cs
+++ Assets/Scripts/Monsters/UpDown.cs
@@ -2,9 +2,14 @@
 {
     public class UpDown : Monster
     {
+        public bool isFacingUp = false;
+
         protected override void OnTurn()
         {
             base.OnTurn();
+            if (isFacingUp) Move(pos.X, pos.Y + 1);
+            else Move(pos.X, pos.Y - 1);
+            isFacingUp = !isFacingUp;
         }
     }
 }

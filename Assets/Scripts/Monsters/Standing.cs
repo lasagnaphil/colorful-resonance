5acc55689ddb365ac9d81576e6846050756e625c
EQwon <bluetwintail0906@gmail.com>
2016-05-05 21:04:42 +0900

    Make Standing Monster

---
+++ Assets/Scripts/Monsters/Standing.cs
@@ -0,0 +1,15 @@
+﻿namespace Monsters
+{
+    public class Standing : Monster
+    {
+        public bool isFacingUp = false;
+
+        protected override void OnTurn()
+        {
+            base.OnTurn();
+            if (isFacingUp) Move(pos.X, pos.Y + 1);
+            else Move(pos.X, pos.Y - 1);
+            isFacingUp = !isFacingUp;
+        }
+    }
+}

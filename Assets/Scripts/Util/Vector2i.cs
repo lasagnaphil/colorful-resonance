270ec5636f0f4258aad5915a8e40c67b12e97a23
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-05-13 13:56:05 +0900

    Added some functions to Vector2i

--- Assets/Scripts/Util/Vector2i.cs
+++ Assets/Scripts/Util/Vector2i.cs
@@ -7,6 +7,18 @@ public struct Vector2i
         this.x = x;
         this.y = y;
     }
-
     public Vector2i(Vector2i vec) : this(vec.x, vec.y) { }
+
+    public static Vector2i operator +(Vector2i v1, Vector2i v2)
+    {
+        return new Vector2i(v1.x + v2.x, v1.y + v2.y);
+    }
+    public static Vector2i operator -(Vector2i v1, Vector2i v2)
+    {
+        return new Vector2i(v1.x - v2.x, v1.y - v2.y);
+    }
+    public static int operator *(Vector2i v1, Vector2i v2)
+    {
+        return v1.x*v2.x + v1.y*v2.y;
+    }
 }

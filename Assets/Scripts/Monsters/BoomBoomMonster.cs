dbe27b780c6e191211b26515e28f992e16a0c308
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-05-16 16:41:34 +0900

    isLoading -> IsLoading in BoomBoomMonster

--- Assets/Scripts/Monsters/BoomBoomMonster.cs
+++ Assets/Scripts/Monsters/BoomBoomMonster.cs
@@ -18,7 +18,7 @@ public class BoomBoomMonster : Monster
     {
         base.OnDestroy();
 
-        if (!GameStateManager.Instance.isLoading)
+        if (!GameStateManager.Instance.IsLoading)
         {
             Direction moveDir = Direction.None;
             GameStateManager.Instance.SpawnProjectile(BombProjectile, pos.X, pos.Y, moveDir);

640e288fe4b658ffc59ad593333cb2a11b44f292
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-05-16 16:03:24 +0900

    Fix missiles from spawning when reloading level (fix)

--- Assets/Scripts/Monsters/HomingMissileMonster.cs
+++ Assets/Scripts/Monsters/HomingMissileMonster.cs
@@ -18,7 +18,7 @@ public class HomingMissileMonster : Monster
     {
         base.OnDestroy();
 
-        if (GameStateManager.Instance.isLoading)
+        if (!GameStateManager.Instance.isLoading)
         {
             Direction moveDir = Direction.Left;
             GameStateManager.Instance.SpawnProjectile(HomingProjectile, pos.X, pos.Y, moveDir);

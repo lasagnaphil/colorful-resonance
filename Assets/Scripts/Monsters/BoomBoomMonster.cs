29b86406dc8eaad79d723c08420072af4960c744
EQwon <bluetwintail0906@gmail.com>
2016-05-16 16:22:53 +0900

    Make Each Scripts

---
+++ Assets/Scripts/Monsters/BoomBoomMonster.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using Utils;
+
+public class BoomBoomMonster : Monster
+{
+    public Projectile BombProjectile;
+
+    protected override Sequence OnTurn()
+    {
+        Sequence sequence = base.OnTurn();
+
+        return sequence;
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        if (!GameStateManager.Instance.isLoading)
+        {
+            Direction moveDir = Direction.None;
+            GameStateManager.Instance.SpawnProjectile(BombProjectile, pos.X, pos.Y, moveDir);
+        }
+    }
+}

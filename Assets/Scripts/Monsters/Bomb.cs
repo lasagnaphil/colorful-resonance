25812a7213fe673f3b7c41337250b43ffa87bc0e
EQwon <bluetwintail0906@gmail.com>
2016-05-16 16:50:09 +0900

    finish Making BoomBoom Monster

--- Assets/Scripts/Monsters/Bomb.cs
+++ Assets/Scripts/Monsters/Bomb.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Utils;
+using DG.Tweening;
 
-public class Bomb : MonoBehaviour {
+public class Bomb : Projectile
+{
+    public TileColor BombColor;
+    public int damage;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private int turn = 0;
+
+    int deltaX;
+    int deltaY;
+
+    protected override Sequence OnTurn()
+    {
+        Sequence sequence = DOTween.Sequence();
+        base.OnTurn();
+        deltaX = player.GetComponent<Position>().X - pos.X;
+        deltaY = player.GetComponent<Position>().Y - pos.Y;
+
+        TileManager.Instance.SetTileColor(pos.X, pos.Y, BombColor);
+        TileManager.Instance.SetTileColor(pos.X + 1, pos.Y, BombColor);
+        TileManager.Instance.SetTileColor(pos.X - 1, pos.Y, BombColor);
+        TileManager.Instance.SetTileColor(pos.X, pos.Y + 1, BombColor);
+        TileManager.Instance.SetTileColor(pos.X + 1, pos.Y + 1, BombColor);
+        TileManager.Instance.SetTileColor(pos.X - 1, pos.Y + 1, BombColor);
+        TileManager.Instance.SetTileColor(pos.X, pos.Y - 1, BombColor);
+        TileManager.Instance.SetTileColor(pos.X + 1, pos.Y - 1, BombColor);
+        TileManager.Instance.SetTileColor(pos.X - 1, pos.Y - 1, BombColor);
+
+        if (Mathf.Abs(deltaX) <= 1 || Mathf.Abs(deltaY) <= 1)
+        {
+            player.ApplyDamage(damage);
+        }
+
+        Destroy(gameObject);
+        return CheckAndDestroy(sequence);
+    }
 }

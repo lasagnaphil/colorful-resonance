41d83029325af5289bdb1b64024f7c065bce2df7
EQwon <bluetwintail0906@gmail.com>
2016-05-18 16:13:20 +0900

    Make Clean Tile Monster

---
+++ Assets/Scripts/Monsters/CleanTileMonster.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using Utils;
+
+public class CleanTileMonster : Monster
+{
+    int deltaX;
+    int deltaY;
+
+    protected override Sequence OnTurn()
+    {
+        Sequence sequence = base.OnTurn();
+        Player player = GameStateManager.Instance.player;
+        Position PlayerPos = player.GetComponent<Position>();
+
+        deltaX = PlayerPos.X - pos.X;
+        deltaY = PlayerPos.Y - pos.Y;
+
+        if (moveCancelled)
+        {
+            moveCancelled = false;
+            return sequence;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            deltaX = deltaX + 1;
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            deltaX = deltaX - 1;
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            deltaY = deltaY - 1;
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            deltaY = deltaY + 1;
+
+        if (deltaX > 0)
+            AnimatedMove(sequence, pos.X + 1, pos.Y);
+        else if (deltaX < 0)
+            AnimatedMove(sequence, pos.X - 1, pos.Y);
+        else if (deltaY > 0)
+            AnimatedMove(sequence, pos.X, pos.Y + 1);
+        else if (deltaY < 0)
+            AnimatedMove(sequence, pos.X, pos.Y - 1);
+
+        return sequence;
+    }
+}
+

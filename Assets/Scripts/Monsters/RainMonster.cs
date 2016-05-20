df67cf508b58f4b53f90333e23a8bee70d7a2017
EQwon <bluetwintail0906@gmail.com>
2016-05-20 13:57:46 +0900

    Making Rain Monster

---
+++ Assets/Scripts/Monsters/RainMonster.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using Utils;
+
+public class RainMonster : Monster
+{
+    public Projectile RainProjectile;
+
+    int RainCoolTime;
+    int Width;
+    int Height;
+    int SpawnPointX;
+    int SpawnPointY;
+
+    protected override Sequence OnTurn()
+    {
+        Sequence sequence = base.OnTurn();
+        
+        if(RainCoolTime < 3)
+            RainCoolTime++;
+        else if (RainCoolTime == 3)
+        {
+            Width = TileManager.Instance.width;
+            Height = TileManager.Instance.height;
+
+            SpawnPointX = Random.Range(1, Width - 1);
+            SpawnPointY = Random.Range(1, Height - 1);
+
+            Direction moveDir = Direction.None;
+            GameStateManager.Instance.SpawnProjectile(RainProjectile, SpawnPointX, SpawnPointY, moveDir);
+            RainCoolTime = 0;
+        }
+
+        return sequence;
+    }
+}
---
+++ Assets/Scripts/Monsters/RainProjectile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utils;
+using DG.Tweening;
+
+public class RainProjectile : Projectile
+{
+    protected override Sequence OnTurn()
+    {
+        Sequence sequence = DOTween.Sequence();
+        base.OnTurn();
+        int n = 0;
+
+        if (n == 1)
+        {
+            Destroy(gameObject);
+            for (int k = 0; k < 3; k++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (TileManager.Instance.GetTileType(pos.X - 1 + k, pos.Y - 1 + j) != TileType.Wall && TileManager.Instance.GetTileType(pos.X - 1 + k, pos.Y - 1 + j) != TileType.None)
+                    {
+                        TileManager.Instance.SetTileColor(pos.X - 1 + k, pos.Y - 1 + j, TileColor.White);
+                    }
+                }
+            }
+        }
+        
+
+        n++;
+        return CheckAndDestroy(sequence);
+    }
+}

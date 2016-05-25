c58273cf071fbc06bedd6ef684250abcacd9a9d7
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-05-25 13:29:09 +0900

    Add JsonHelper class
    Serialize/Deserialize types with fullserializer

---
+++ Assets/Scripts/Util/JsonHelper.cs
@@ -0,0 +1,30 @@
+﻿using FullSerializer;
+
+namespace Utils
+{
+    public class JsonHelper
+    {
+        private static readonly fsSerializer _serializer = new fsSerializer();
+
+        public static string Serialize<T>(object value, bool pretty = true, bool warnings = true)
+        {
+            fsData data;
+            if (warnings) _serializer.TrySerialize(typeof (T), value, out data).AssertSuccess();
+            else _serializer.TrySerialize(typeof (T), value, out data).AssertSuccessWithoutWarnings();
+
+            if (pretty) return fsJsonPrinter.PrettyJson(data);
+            else return fsJsonPrinter.CompressedJson(data);
+        }
+
+        public static object Deserialize<T>(object value, string serializedState, bool warnings = true)
+        {
+            fsData data = fsJsonParser.Parse(serializedState);
+            object deserialized = null;
+            if (warnings) _serializer.TryDeserialize(data, typeof (T), ref deserialized).AssertSuccess();
+            else _serializer.TryDeserialize(data, typeof (T), ref deserialized).AssertSuccessWithoutWarnings();
+            
+            return deserialized;
+        }
+
+    }
+}

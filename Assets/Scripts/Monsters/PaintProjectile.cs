cca9b217e6670f6e2dbb4739680e713510f92c56
EQwon <bluetwintail0906@gmail.com>
2016-06-03 16:05:43 +0900

    Make New Map 3-2

--- Assets/Scripts/Monsters/PaintProjectile.cs
+++ Assets/Scripts/Monsters/PaintProjectile.cs
@@ -12,14 +12,14 @@ public class PaintProjectile : Projectile
         Sequence sequence = DOTween.Sequence();
         base.OnTurn();
 
-        if (TileManager.Instance.GetTileType(pos.X, pos.Y) == TileType.Wall && Type != ProjectileType.GoThrough)
+        if ((TileManager.Instance.GetTileType(pos.X, pos.Y) == TileType.Wall || TileManager.Instance.GetTileType(pos.X, pos.Y) == TileType.None) && Type != ProjectileType.GoThrough)
         {
             Destroy(gameObject);
             for(int k = 0; k < 3; k++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    if (TileManager.Instance.GetTileType(pos.X - 1 + k, pos.Y - 1 + j) != TileType.Wall && TileManager.Instance.GetTileType(pos.X - 1 + k, pos.Y - 1 + j) != TileType.None)
+                    if (TileManager.Instance.GetTileType(pos.X - 1 + k, pos.Y - 1 + j) != TileType.None)
                     {
                         TileManager.Instance.SetTileColor(pos.X - 1 + k, pos.Y - 1 + j, BombColor);
                     }

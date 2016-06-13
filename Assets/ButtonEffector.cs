252fb017bcb321a1e86669d503ee204082de7d28
Minjung Kim <Reny.atkins@gmail.com>
2016-06-13 23:18:06 +0900

    Change select button layout

--- Assets/ButtonEffector.cs
+++ Assets/ButtonEffector.cs
@@ -11,22 +11,29 @@ public class ButtonEffector : MonoBehaviour {
 	GameObject baseEffect;
 	GameObject highlightEffect;
 	Image buttonImage;
+	GameObject textObject;
 
 	// Use this for initialization
 	void Start () {
 		buttonImage = gameObject.GetComponent<Image>();
+		textObject = gameObject.transform.Find("Text").gameObject;
 
 		int colorIndex = (int)color;
-		baseEffect = Instantiate(baseEffects[colorIndex], transform.position + new Vector3(0,0,1), Quaternion.identity) as GameObject;
+		baseEffect = Instantiate(baseEffects[colorIndex], transform.position, Quaternion.identity) as GameObject;
+		baseEffect.transform.parent = gameObject.transform;
+		baseEffect.transform.localPosition += new Vector3(0,-40f,-30f);
+		baseEffect.transform.localScale = new Vector3(1, 1, 1);
 		highlightEffect = Instantiate(highlightEffects[colorIndex], transform.position, Quaternion.identity) as GameObject;
+		highlightEffect.transform.parent = gameObject.transform;
 		highlightEffect.SetActive(false);
+		textObject.transform.localPosition += new Vector3(0,0,-60f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (gameObject == EventSystem.current.currentSelectedGameObject)
 		{
-			buttonImage.color = new Color(1, 1, 1, 1);
+			// buttonImage.color = new Color(1, 1, 1, 1);
 
 			if (highlightEffect.activeInHierarchy == false)
 				highlightEffect.SetActive(true);
@@ -36,7 +43,7 @@ public class ButtonEffector : MonoBehaviour {
 		}
 		else
 		{
-			buttonImage.color = new Color(0.6f, 0.6f, 0.6f, 1);
+			// buttonImage.color = new Color(0.6f, 0.6f, 0.6f, 1);
 
 			if (highlightEffect.activeInHierarchy == true)
 				highlightEffect.SetActive(false);

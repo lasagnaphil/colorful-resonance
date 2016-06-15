3d6bd64b21f9f62fe23e308faf2cc3297bcfc7ea
Minjung Kim <Reny.atkins@gmail.com>
2016-06-16 05:42:32 +0900

    Add opening sound

---
+++ Assets/Scripts/MainSoundPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainSoundPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+
+		if (GameObject.Find("MainSoundPlayer") != null)
+			Destroy(gameObject);
+		else
+			gameObject.name = "MainSoundPlayer";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (SceneManager.GetActiveScene().name == "Game")
+			Destroy(gameObject);	
+	}
+}

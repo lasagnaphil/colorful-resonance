d33b1f466dd22101308b6cc0a2d9b238878e9d54
Minjung Kim <Reny.atkins@gmail.com>
2016-06-16 03:57:37 +0900

    Fix credit scene

--- Assets/GoToMainButton.cs
+++ Assets/GoToMainButton.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class GoToMainButton : MonoBehaviour {
 
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return))
+			GoToMain();
+	}
+
 	public void GoToMain()
 	{
 		SceneManager.LoadScene("Main");

f5e4ea2ba8cd89f5e047fe734e77366d8dbc0da1
Minjung Kim <Reny.atkins@gmail.com>
2016-06-15 18:01:37 +0900

    Monster can't clean tile under the orb

--- Assets/Scripts/Monsters/RainProjectile.cs
+++ Assets/Scripts/Monsters/RainProjectile.cs
@@ -24,7 +24,9 @@ public class RainProjectile : Projectile
         {
             for (int j = 0; j < 3; j++)
             {
-                if (TileManager.Instance.GetTileType(pos.X - 1 + k, pos.Y - 1 + j) != TileType.Wall && TileManager.Instance.GetTileType(pos.X - 1 + k, pos.Y - 1 + j) != TileType.None)
+                if (TileManager.Instance.GetTileType(pos.X - 1 + k, pos.Y - 1 + j) != TileType.Wall &&
+                    TileManager.Instance.GetTileType(pos.X - 1 + k, pos.Y - 1 + j) != TileType.None &&
+                    GameStateManager.Instance.CheckOrbPosition(pos.X - 1 + k, pos.Y - 1 + j) == null)
                 {
                     TileManager.Instance.SetTileColor(pos.X - 1 + k, pos.Y - 1 + j, BombColor);
                     Destroy(Instantiate(effectObject, TileManager.Instance.GetTile(pos.X - 1 + k, pos.Y - 1 + j).transform.position + new Vector3(0,0,-1), Quaternion.identity) as GameObject, 2);

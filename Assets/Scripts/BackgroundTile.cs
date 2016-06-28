bc136c54fa53941caea6d9a138c89f74a1bb230c
Minjung Kim <Reny.atkins@gmail.com>
2016-06-28 23:55:39 +0900

    Minor fix 0 pivot & size

--- Assets/Scripts/BackgroundTile.cs
+++ Assets/Scripts/BackgroundTile.cs
@@ -16,8 +16,41 @@ public class BackgroundTile : MonoBehaviour {
 
 	void Start () {
 		if (type == "color") StartCoroutine(RepeatFade());
+		else if (type == "gray") StartCoroutine(RepeatFadeGray());
 	}
 	
+	IEnumerator RepeatFadeGray()
+	{
+		int remain6 = (((int)transform.position.x+20) + ((int)transform.position.y+20)) % 6;
+		
+		if (remain6 == 0)
+			yield return null;
+		else if (remain6 == 1 || remain6 == 5)
+			yield return new WaitForSeconds(delay*5);
+		else if (remain6 == 2 || remain6 == 4)
+			yield return new WaitForSeconds(delay*5*2);
+		else if (remain6 == 3)
+			yield return new WaitForSeconds(delay*5*3);
+
+		while (true)
+		{
+			for (int i = 0; i < 20; i++)
+			{
+				spriteRenderer.color -= new Color((255*0.5f)/(20f*255f), 
+											(255*0.5f)/(20f*255f), 
+											(255*0.5f)/(20f*255f), 0);
+				yield return new WaitForSeconds(delay);
+			}
+			for (int i = 0; i < 20; i++)
+			{
+				spriteRenderer.color += new Color((255*0.5f)/(20f*255f), 
+											(255*0.5f)/(20f*255f), 
+											(255*0.5f)/(20f*255f), 0);
+				yield return new WaitForSeconds(delay);
+			}
+		}
+	}
+
 	IEnumerator RepeatFade()
 	{
 		// float delta = (1.0f - minAlphaValue)/20f;

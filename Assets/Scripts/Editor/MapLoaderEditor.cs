781a1ffd422af80771e12ef37a6a5ab0adb6c417
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-07-03 23:34:40 +0900

    Create editor script for MapLoader (for easier map insertion)

---
+++ Assets/Scripts/Editor/MapLoaderEditor.cs
@@ -0,0 +1,25 @@
+﻿using Rotorz.ReorderableList;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapLoader))]
+public class MapLoaderEditor : Editor
+{
+    private SerializedProperty mapAssetListProperty;
+    private SerializedProperty mapToLoadProperty;
+
+    void OnEnable()
+    {
+        mapAssetListProperty = serializedObject.FindProperty("mapAssetList");
+        mapToLoadProperty = serializedObject.FindProperty("mapToLoad");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(mapToLoadProperty);
+        EditorGUILayout.LabelField("Map Asset List:");
+        ReorderableListGUI.ListField(mapAssetListProperty);
+        serializedObject.ApplyModifiedProperties();
+    }
+}

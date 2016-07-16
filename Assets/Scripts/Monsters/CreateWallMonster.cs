cebc9d73a3f1849dacf078cefc7a0c793226815e
EQ1 <bluetwintail@naver.com>
2016-07-16 16:56:20 +0900

    Try To Make CreateWall Monster

--- Assets/Scripts/Monsters/CreateWallMonster.cs
+++ Assets/Scripts/Monsters/CreateWallMonster.cs
@@ -7,24 +7,8 @@ public class CreateWallMonster : Monster
 {
     public Projectile MakeWallProjectile;
 
-    int SpawnPointX;
-    int SpawnPointY;
-
     protected override void OnTurn(Sequence sequence)
     {
-        if (destroyed)
-        {
-            SpawnPointX = GameStateManager.Instance.player.GetComponent<Position>().X;
-            SpawnPointY = GameStateManager.Instance.player.GetComponent<Position>().Y;
 
-            Direction moveDir = Direction.None;
-            SpawnProjectile(MakeWallProjectile, SpawnPointX, SpawnPointY, moveDir);
-        }
-    }
-
-    protected override void WhenDestroyed()
-    {
-        TileManager.Instance.SetTileType(pos.X, pos.Y, TileType.Wall);
-        TileManager.Instance.SetTileColor(pos.X, pos.Y, TileColor.White);
     }
 }

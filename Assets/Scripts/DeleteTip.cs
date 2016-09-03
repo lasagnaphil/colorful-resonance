cdf40cea5a5bd181997990fec45a1d12f37baae2
EQwon <bluetwintail@naver.com>
2016-09-03 23:50:35 +0900

    Delete Tip After Stage 1

---
+++ Assets/Scripts/DeleteTip.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DeleteTip : MonoBehaviour
+{
+	public GameObject GameManager;
+
+	private string MapToLoad;
+
+	void Update()
+	{
+		MapToLoad = GameManager.GetComponent<MapLoader> ().MapToLoad;
+
+		if (MapToLoad != "1-1" && MapToLoad != "1-2" && MapToLoad != "1-3" && MapToLoad != "1-04"
+		   && MapToLoad != "1-05" && MapToLoad != "1-06" && MapToLoad != "1-07" && MapToLoad != "1-08"
+		   && MapToLoad != "1-09" && MapToLoad != "1-10")
+		{
+			Destroy (gameObject);
+		}
+	}
+}

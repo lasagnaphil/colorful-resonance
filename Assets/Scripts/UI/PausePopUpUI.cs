5a7884b35c6b33a810a4e3a5d3b090e9191a6ab2
EQwon <bluetwintail@naver.com>
2016-09-07 14:28:15 +0900

    Make Pause UI by using Dummy Pause Button

---
+++ Assets/Scripts/UI/PausePopUpUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using Utils;
+
+public class PausePopUpUI : MonoBehaviour
+{
+	bool isCoroutinePlaying = false;
+
+	public void GoToMain()
+	{
+		StartCoroutine(GoToScene ("Main"));
+	}
+
+	public void Restart()
+	{
+		// 재시작 코드
+	}
+
+	IEnumerator GoToScene(string sceneName)
+	{
+		isCoroutinePlaying = true;
+		FadeEffectCanvas fadeEffectCanvas = FindObjectOfType<FadeEffectCanvas>();
+		IEnumerator coroutine = fadeEffectCanvas.PlayFadeOutEffect();
+		yield return StartCoroutine(coroutine);
+		isCoroutinePlaying = false;
+		SceneManager.LoadScene(sceneName);
+	}
+}

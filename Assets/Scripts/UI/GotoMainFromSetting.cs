820c076bd6471daf39576165d3c628bad011edf1
EQwon <bluetwintail@naver.com>
2016-09-07 14:45:16 +0900

    Add Go Back Button to SetUp UI

---
+++ Assets/Scripts/UI/GotoMainFromSetting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GotoMainFromSetting : MonoBehaviour
+{
+	bool isCoroutinePlaying = false;
+
+	public void GoToMain()
+	{
+		StartCoroutine(GoToScene ("Main"));
+	}
+
+	IEnumerator GoToScene(string sceneName)
+	{
+		isCoroutinePlaying = true;
+		FadeEffectCanvas fadeEffectCanvas = FindObjectOfType<FadeEffectCanvas>();
+		IEnumerator coroutine = fadeEffectCanvas.PlayFadeOutEffect();
+		yield return StartCoroutine(coroutine);
+		isCoroutinePlaying = false;
+		SceneManager.LoadScene(sceneName);
+	}
+}

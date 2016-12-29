39b234ff98fc26d92b868fca5418f4c3fa5cdb83
EQwon <bluetwintail@naver.com>
2016-12-30 04:33:09 +0900

    Stage 하나씩 열리도록 바꿈

--- Assets/Scripts/MapDataManager.cs
+++ Assets/Scripts/MapDataManager.cs
@@ -1,8 +1,164 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapDataManager : MonoBehaviour
 {
+    public GameObject[] Stage;
+    public Sprite UnlockTile;
 
+    void Start()
+    {
+        if (PlayerPrefs.GetString("1-1 Clear") == "Yes")
+        {
+            Stage[1].GetComponent<Image>().sprite = UnlockTile;
+            Stage[1].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("1-2 Clear") == "Yes")
+        {
+            Stage[2].GetComponent<Image>().sprite = UnlockTile;
+            Stage[2].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("1-3 Clear") == "Yes")
+        {
+            Stage[3].GetComponent<Image>().sprite = UnlockTile;
+            Stage[3].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("1-4 Clear") == "Yes")
+        {
+            Stage[4].GetComponent<Image>().sprite = UnlockTile;
+            Stage[4].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("1-5 Clear") == "Yes")
+        {
+            Stage[5].GetComponent<Image>().sprite = UnlockTile;
+            Stage[5].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("1-6 Clear") == "Yes")
+        {
+            Stage[6].GetComponent<Image>().sprite = UnlockTile;
+            Stage[6].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("1-7 Clear") == "Yes")
+        {
+            Stage[7].GetComponent<Image>().sprite = UnlockTile;
+            Stage[7].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("1-8 Clear") == "Yes")
+        {
+            Stage[8].GetComponent<Image>().sprite = UnlockTile;
+            Stage[8].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("1-9 Clear") == "Yes")
+        {
+            Stage[9].GetComponent<Image>().sprite = UnlockTile;
+            Stage[9].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("1-10 Clear") == "Yes")
+        {
+            Stage[10].GetComponent<Image>().sprite = UnlockTile;
+            Stage[10].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("2-1 Clear") == "Yes")
+        {
+            Stage[11].GetComponent<Image>().sprite = UnlockTile;
+            Stage[11].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("2-2 Clear") == "Yes")
+        {
+            Stage[12].GetComponent<Image>().sprite = UnlockTile;
+            Stage[12].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("2-3 Clear") == "Yes")
+        {
+            Stage[13].GetComponent<Image>().sprite = UnlockTile;
+            Stage[13].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("2-4 Clear") == "Yes")
+        {
+            Stage[14].GetComponent<Image>().sprite = UnlockTile;
+            Stage[14].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("2-5 Clear") == "Yes")
+        {
+            Stage[15].GetComponent<Image>().sprite = UnlockTile;
+            Stage[15].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("2-6 Clear") == "Yes")
+        {
+            Stage[16].GetComponent<Image>().sprite = UnlockTile;
+            Stage[16].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("3-1 Clear") == "Yes")
+        {
+            Stage[17].GetComponent<Image>().sprite = UnlockTile;
+            Stage[17].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("3-2 Clear") == "Yes")
+        {
+            Stage[18].GetComponent<Image>().sprite = UnlockTile;
+            Stage[18].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("3-3 Clear") == "Yes")
+        {
+            Stage[19].GetComponent<Image>().sprite = UnlockTile;
+            Stage[19].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("3-4 Clear") == "Yes")
+        {
+            Stage[20].GetComponent<Image>().sprite = UnlockTile;
+            Stage[20].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("3-5 Clear") == "Yes")
+        {
+            Stage[21].GetComponent<Image>().sprite = UnlockTile;
+            Stage[21].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("4-1 Clear") == "Yes")
+        {
+            Stage[22].GetComponent<Image>().sprite = UnlockTile;
+            Stage[22].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("4-2 Clear") == "Yes")
+        {
+            Stage[23].GetComponent<Image>().sprite = UnlockTile;
+            Stage[23].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("4-3 Clear") == "Yes")
+        {
+            Stage[24].GetComponent<Image>().sprite = UnlockTile;
+            Stage[24].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("4-4 Clear") == "Yes")
+        {
+            Stage[25].GetComponent<Image>().sprite = UnlockTile;
+            Stage[25].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("4-5 Clear") == "Yes")
+        {
+            Stage[26].GetComponent<Image>().sprite = UnlockTile;
+            Stage[26].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("5-1 Clear") == "Yes")
+        {
+            Stage[27].GetComponent<Image>().sprite = UnlockTile;
+            Stage[27].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("5-2 Clear") == "Yes")
+        {
+            Stage[28].GetComponent<Image>().sprite = UnlockTile;
+            Stage[28].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("5-3 Clear") == "Yes")
+        {
+            Stage[29].GetComponent<Image>().sprite = UnlockTile;
+            Stage[29].GetComponent<Button>().enabled = true;
+        }
+        if (PlayerPrefs.GetString("5-4 Clear") == "Yes")
+        {
+            Stage[30].GetComponent<Image>().sprite = UnlockTile;
+            Stage[30].GetComponent<Button>().enabled = true;
+        }
+    }
 }
--- Assets/Scripts/States/GameStateWin.cs
+++ Assets/Scripts/States/GameStateWin.cs
@@ -5,10 +5,114 @@ namespace States
     public class GameStateWin : IGameState
     {
         private GameObject GameManager;
+        private string MapName;
 
         public void Enter(GameStateManager gsm)
         {
-            Debug.Log(GameObject.Find("GameManager").GetComponent<MapLoader>().MapToLoad);
+            MapName = GameObject.Find("GameManager").GetComponent<MapLoader>().MapToLoad;
+
+            switch (MapName)
+            {
+                case "1-1" :
+                    PlayerPrefs.SetString("1-1 Clear", "Yes");
+                    break;
+                case "1-2":
+                    PlayerPrefs.SetString("1-2 Clear", "Yes");
+                    break;
+                case "1-3":
+                    PlayerPrefs.SetString("1-3 Clear", "Yes");
+                    break;
+                case "1-04":
+                    PlayerPrefs.SetString("1-4 Clear", "Yes");
+                    break;
+                case "1-05":
+                    PlayerPrefs.SetString("1-5 Clear", "Yes");
+                    break;
+                case "1-06":
+                    PlayerPrefs.SetString("1-6 Clear", "Yes");
+                    break;
+                case "1-07":
+                    PlayerPrefs.SetString("1-7 Clear", "Yes");
+                    break;
+                case "1-08":
+                    PlayerPrefs.SetString("1-8 Clear", "Yes");
+                    break;
+                case "1-09":
+                    PlayerPrefs.SetString("1-9 Clear", "Yes");
+                    break;
+                case "1-10":
+                    PlayerPrefs.SetString("1-10 Clear", "Yes");
+                    break;
+                case "2-01":
+                    PlayerPrefs.SetString("2-1 Clear", "Yes");
+                    break;
+                case "2-02":
+                    PlayerPrefs.SetString("2-2 Clear", "Yes");
+                    break;
+                case "2-03":
+                    PlayerPrefs.SetString("2-3 Clear", "Yes");
+                    break;
+                case "2-04":
+                    PlayerPrefs.SetString("2-4 Clear", "Yes");
+                    break;
+                case "2-05":
+                    PlayerPrefs.SetString("2-5 Clear", "Yes");
+                    break;
+                case "2-06":
+                    PlayerPrefs.SetString("2-6 Clear", "Yes");
+                    break;
+                case "2-07":
+                    PlayerPrefs.SetString("2-7 Clear", "Yes");
+                    break;
+                case "3-1":
+                    PlayerPrefs.SetString("3-1 Clear", "Yes");
+                    break;
+                case "3-2":
+                    PlayerPrefs.SetString("3-2 Clear", "Yes");
+                    break;
+                case "3-3":
+                    PlayerPrefs.SetString("3-3 Clear", "Yes");
+                    break;
+                case "3-4":
+                    PlayerPrefs.SetString("3-4 Clear", "Yes");
+                    break;
+                case "3-5":
+                    PlayerPrefs.SetString("3-5 Clear", "Yes");
+                    break;
+                case "4-1":
+                    PlayerPrefs.SetString("4-1 Clear", "Yes");
+                    break;
+                case "4-2":
+                    PlayerPrefs.SetString("4-2 Clear", "Yes");
+                    break;
+                case "4-3":
+                    PlayerPrefs.SetString("4-3 Clear", "Yes");
+                    break;
+                case "4-4":
+                    PlayerPrefs.SetString("4-4 Clear", "Yes");
+                    break;
+                case "4-5":
+                    PlayerPrefs.SetString("4-5 Clear", "Yes");
+                    break;
+                case "5-1":
+                    PlayerPrefs.SetString("5-1 Clear", "Yes");
+                    break;
+                case "5-2":
+                    PlayerPrefs.SetString("5-2 Clear", "Yes");
+                    break;
+                case "5-3":
+                    PlayerPrefs.SetString("5-3 Clear", "Yes");
+                    break;
+                case "5-4":
+                    PlayerPrefs.SetString("5-4 Clear", "Yes");
+                    break;
+                case "5-5":
+                    PlayerPrefs.SetString("5-5 Clear", "Yes");
+                    break;
+                default:
+                    Debug.Log("Game State Win is Something Error");
+                    break;
+            }
         }
 
         public void Update(GameStateManager gsm)

f9fff82b4dcafa70d3b785e5a579bcc458fafa6c
EQwon <bluetwintail@naver.com>
2017-01-02 00:45:15 +0900

    사운드 컨트롤러를 추가하는 중

---
+++ Assets/Scripts/SetUp/SoundController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public void BGMOn()
+    {
+        Debug.Log("BGM On");
+    }
+
+    public void BGMOff()
+    {
+        Debug.Log("BGM Off");
+    }
+
+    public void SEOn()
+    {
+        Debug.Log("SE On");
+    }
+
+    public void SEOff()
+    {
+        Debug.Log("SE Off");
+    }
+}

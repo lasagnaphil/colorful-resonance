ca9110e8e1779abbc63e619b762ff61456645b01
EQwon <bluetwintail@naver.com>
2017-01-02 00:51:23 +0900

    소리 On/Off 누르면 색 바뀌게 설정

--- Assets/Scripts/SetUp/SoundController.cs
+++ Assets/Scripts/SetUp/SoundController.cs
@@ -1,26 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundController : MonoBehaviour
 {
-    public void BGMOn()
+    public Text BGMOn;
+    public Text BGMOff;
+    public Text SEOn;
+    public Text SEOff;
+
+    public void MakeBGMOn()
     {
+        BGMOn.color = new Color(0.2f, 0.2f, 0.2f);
+        BGMOff.color = new Color(1f, 1f, 1f);
         Debug.Log("BGM On");
     }
 
-    public void BGMOff()
+    public void MakeBGMOff()
     {
+        BGMOff.color = new Color(0.2f, 0.2f, 0.2f);
+        BGMOn.color = new Color(1f, 1f, 1f);
         Debug.Log("BGM Off");
     }
 
-    public void SEOn()
+    public void MakeSEOn()
     {
+        SEOn.color = new Color(0.2f, 0.2f, 0.2f);
+        SEOff.color = new Color(1f, 1f, 1f);
         Debug.Log("SE On");
     }
 
-    public void SEOff()
+    public void MakeSEOff()
     {
+        SEOff.color = new Color(0.2f, 0.2f, 0.2f);
+        SEOn.color = new Color(1f, 1f, 1f);
         Debug.Log("SE Off");
     }
 }
